1c1f9d57d96fa5060302611187f782e57cf39e99
AdhiKrisna <krisnahmbtn@gmail.com>
2025-06-10 01:31:42 +0700

    cors add allowedOrigins

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ const allowedOrigins = [
     "http://localhost:5173",
     "http://localhost:5000",
     "http://localhost:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
     // "https://a-09-450915.uc.r.appspot.com"
 ];
 app.use(cors({
