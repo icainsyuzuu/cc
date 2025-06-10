0f29d702d703d53edfe3958c6824bc9806eec16b
applephii <khaqiki@gmail.com>
2025-06-10 08:28:16 +0700

    add allowedOrigins

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "http://localhost:8080",
     // "https://a-09-450915.uc.r.appspot.com"
 ];
 app.use(cors({
