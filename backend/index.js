f648a039601056aac39fd7ee4153b8dc85229b7b
stroberinanas <sabilakhairina2@gmail.com>
2025-06-11 08:01:22 +0700

    rev index

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5000",
-    "http://localhost:3000",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:8080",
+    // "http://localhost:5173",
+    // "http://localhost:5000",
+    // "http://localhost:3000",
+    // "http://localhost:5500",
+    // "http://127.0.0.1:5500",
+    // "http://localhost:8080",
     // "https://a-09-450915.uc.r.appspot.com"
+    "https:h-08-451505.uc.r.appspot.com"
 ];
 app.use(cors({
     origin: allowedOrigins,
